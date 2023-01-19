7e8947cecfc1f4200c0d4f237a5f5684a8575186
Ashik Eqbal <ashikeqbal30@gmail.com>
2023-01-19 19:29:26 +0530

    meta tag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import { createRoot } from 'react-dom/client';
+import { hydrate, render } from "react-dom";
 import App from './App';
 import './index.css';
 
-const rootElement = document.getElementById('root');
-createRoot(rootElement).render(<App />);
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
