4790485f14e223a72c938d89fa185ad03bb84428
Ashik Eqbal <ashikeqbal30@gmail.com>
2023-02-24 21:07:13 +0530

    firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { Navbar, FooterGit } from './components';
 import { Home } from './container/index'
 import { SingleBlogPost, Error, Blog } from './Pages';
 import './App.scss'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 
 
 const App = () => {
@@ -23,5 +26,18 @@ const App = () => {
   )
 };
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAwzHmK37rqD0VJ4lJje5RftNehBscTsNU",
+  authDomain: "itsashik-info.firebaseapp.com",
+  projectId: "itsashik-info",
+  storageBucket: "itsashik-info.appspot.com",
+  messagingSenderId: "307624946048",
+  appId: "1:307624946048:web:48ac209d93f476fbda3965",
+  measurementId: "G-FY3432HZ50"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export default App;
