53d244248966dc6c0c2d0cfec7107a62b1682ebb
Ashik Eqbal <hello@itsashik.info>
2025-01-28 03:42:18 +0530

    feat: update favicon sizes in layout for improved display on various devices

--- app/layout.tsx
+++ app/layout.tsx
@@ -54,8 +54,7 @@ export const metadata = {
   icons: {
     icon: [
       { url: '/favicon.ico' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
     ],
     apple: [
       { url: '/apple-touch-icon.png' },
